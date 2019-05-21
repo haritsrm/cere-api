1c803273a815776407c10119d97f4ce7a7d4ff3a
rietzche <haritzrahman98@gmail.com>
2019-05-21 23:51:40 +0700

    add cerevid and cereout migration

--- database/migrations/2019_05_21_164015_create_cereouts_table.php
+++ database/migrations/2019_05_21_164015_create_cereouts_table.php
@@ -17,13 +17,13 @@ class CreateCereoutsTable extends Migration
             $table->bigIncrements('id');
             $table->bigInteger('tryout_id');
             $table->bigInteger('user_id');
-            $table->bigInteger('my_time');
-            $table->integer('score');
-            $table->integer('total_answer');
-            $table->integer('correct_answered');
-            $table->integer('incorrect_answered');
-            $table->integer('left_answered');
-            $table->string('result_status');
+            $table->bigInteger('my_time')->nullable();
+            $table->integer('score')->nullable();
+            $table->integer('total_answer')->nullable();
+            $table->integer('correct_answered')->nullable();
+            $table->integer('incorrect_answered')->nullable();
+            $table->integer('left_answered')->nullable();
+            $table->string('result_status')->nullable();
             $table->timestamps();
         });
     }
