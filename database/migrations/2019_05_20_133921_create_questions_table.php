3c8e7d582e606d924461dc91e0454f36a1d3740d
Rifqi Ardian <rifardian@gmail.com>
2019-05-22 21:49:45 +0700

    edit login

---
+++ database/migrations/2019_05_20_133921_create_questions_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('tryout_id');
+            $table->string('question');
+            $table->string('option_a');
+            $table->string('option_b');
+            $table->string('option_c');
+            $table->string('option_d');
+            $table->string('option_e')->nullable();
+            $table->string('option_f')->nullable();
+            $table->string('correct_answer');
+            $table->longText('explanation');
+            $table->string('url_explanation');
+            $table->integer('correct_score');
+            $table->integer('incorrect_score');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
