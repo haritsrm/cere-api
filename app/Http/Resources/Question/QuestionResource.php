ce66f09c0a59f710ef36fa4972c4c35aa2add1d1
Rifqi Ardian <rifardian@gmail.com>
2019-05-23 22:59:53 +0700

    edit response question

--- app/Http/Resources/Question/QuestionResource.php
+++ app/Http/Resources/Question/QuestionResource.php
@@ -22,12 +22,12 @@ class QuestionResource extends JsonResource
             'duration' => $this->duration,
             'question' => $this->question,
             'option' => [
-                'option_a' => $this->option_a,
-                'option_b' => $this->option_b,
-                'option_c' => $this->option_c,
-                'option_d' => $this->option_d,
-                'option_e' => $this->option_e,
-                'option_f' => $this->option_f
+                ['option_a' => $this->option_a],
+                ['option_b' => $this->option_b],
+                ['option_c' => $this->option_c],
+                ['option_d' => $this->option_d],
+                ['option_e' => $this->option_e],
+                ['option_f' => $this->option_f]
             ],
             'correct_answer' => $this->correct_answer,
             'explanation' => $this->explanation,
