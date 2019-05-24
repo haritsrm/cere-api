5e5fbd9951454461a1f5f748e9f9152c70814b37
rietzche <haritzrahman98@gmail.com>
2019-05-25 00:45:21 +0700

    add teacher on field course

--- app/Http/Resources/Course/CourseResource.php
+++ app/Http/Resources/Course/CourseResource.php
@@ -5,6 +5,7 @@ namespace App\Http\Resources\Course;
 use Illuminate\Http\Resources\Json\JsonResource;
 use App\Models\Lesson;
 use App\User;
+use App\Models\Favorite;
 
 class CourseResource extends JsonResource
 {
@@ -16,7 +17,15 @@ class CourseResource extends JsonResource
      */
     public function toArray($request)
     {
+        $learned = false;
         $lesson = Lesson::find($this->lesson_id);
+        try {
+            $favorite_result = Favorite::where('user_id', $request->user()->id)
+                                ->where('course_id', $this->id)->first()->id;
+        }
+        catch(ErrorException $e) {
+            $favorite_result = 0;
+        }
 
         return [
             'id' => $this->id,
@@ -28,6 +37,9 @@ class CourseResource extends JsonResource
                 'class_id' => $lesson->class_id,
                 'name' => $lesson->name,
                 'passing_percentage' => $lesson->passing_percentage
+            ],
+            'favorite' => [
+                'id' => $favorite_result,
             ],
             'teacher' => [
                 'name' => User::find($this->user_id)->name,
