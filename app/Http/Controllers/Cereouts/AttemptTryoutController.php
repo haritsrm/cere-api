2b271c0304f734f0a1533fda2dd08bce63cd91fe
Rifqi Ardian <rifardian@gmail.com>
2019-05-27 02:15:58 +0700

    add get tryout and expire tryout by id user

---
+++ app/Http/Controllers/Cereouts/AttemptTryoutController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Cereouts;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\AttemptTryout;
+use App\Models\Tryout;
+use Carbon\Carbon;
+use App\Http\Resources\Tryout\TryoutCollection;
+
+class AttemptTryoutController extends Controller
+{
+    //
+    public function getTryoutUser($id){
+    	$today =  Carbon::now()->todatestring();
+    	$data = AttemptTryout::join('tryouts','tryouts.id','=','attempt_tryouts.tryout_id')
+    		->select('tryouts.*', 'attempt_tryouts.user_id as user_id')
+    		->where('attempt_tryouts.user_id','=',$id)
+    		->where('tryouts.end_date','>=',$today)
+    		->get();
+    	return TryoutCollection::collection($data);	
+    }
+
+    public function getExpireTryoutUser($id){
+    	$today =  Carbon::now()->todatestring();
+    	$data = AttemptTryout::join('tryouts','tryouts.id','=','attempt_tryouts.tryout_id')
+    		->select('tryouts.*', 'attempt_tryouts.user_id as user_id')
+    		->where('attempt_tryouts.user_id','=',$id)
+    		->where('tryouts.end_date','<=',$today)
+    		->get();
+    	return TryoutCollection::collection($data);	
+    }
+
+}
