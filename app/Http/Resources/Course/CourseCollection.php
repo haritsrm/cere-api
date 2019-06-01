2477a1e238516ed9dcd2d3f84bf30d7f54b7d92c
rietzche <haritzrahman98@gmail.com>
2019-06-02 06:38:09 +0700

    modify last seen on collection

--- app/Http/Resources/Course/CourseCollection.php
+++ app/Http/Resources/Course/CourseCollection.php
@@ -36,7 +36,7 @@ class CourseCollection extends JsonResource
                 'link' => route('course/detail', $this->id),
             ],
             'rating' => round($this->reviews()->avg('star')),
-            'last_seen' => ($this->lastSeen()->where('user_id', $request->user()->id)->first()->updated_at->diffForHumans() ? $this->lastSeen()->where('user_id', $request->user()->id)->first()->updated_at->diffForHumans() : null),
+            'last_seen' => (!is_null($this->lastSeen()->where('user_id', $request->user()->id)->first()) ? $this->lastSeen()->where('user_id', $request->user()->id)->first()->updated_at->diffForHumans() : null),
         ];
     }
 }
