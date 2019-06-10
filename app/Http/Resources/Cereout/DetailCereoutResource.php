a7fda86937964b36679dd9d023d3178fa9aa0152
Rifqi Ardian <rifardian@gmail.com>
2019-06-11 00:14:27 +0700

    edit response detail tryout

--- app/Http/Resources/Cereout/DetailCereoutResource.php
+++ app/Http/Resources/Cereout/DetailCereoutResource.php
@@ -36,6 +36,11 @@ class DetailCereoutResource extends JsonResource
             'discussion' => [
                 'question_id' => $this->question_id,
                 'question' => $this->question,
+                'option_a' => $this->option_a,
+                'option_b' => $this->option_b,
+                'option_c' => $this->option_c,
+                'option_d' => $this->option_d,
+                'correct_answer' => $this->correct_answer,
                 'explanation' => $this->explanation,
                 'url_explanation' => $this->url_explanation
             ],
