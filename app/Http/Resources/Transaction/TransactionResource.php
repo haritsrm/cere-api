a0ccabaff74ae06c64a5f5e0bb144b80cb39fcf0
Rifqi Ardian <rifardian@gmail.com>
2019-06-19 01:41:04 +0700

    edit response transaction

--- app/Http/Resources/Transaction/TransactionResource.php
+++ app/Http/Resources/Transaction/TransactionResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources\Transaction;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 use App\Models\Membership;
+use App\Models\NominalTopUp;
 
 class TransactionResource extends JsonResource
 {
@@ -15,16 +16,24 @@ class TransactionResource extends JsonResource
      */
     public function toArray($request)
     {
-        if($this->membership_id!=null){
-            $membership = Membership::where('id','=',$this->membership_id)->first();
-            $membership_name = $membership->name;
-        }else{
-            $membership_name = null;
+        $membership_name = null;
+        $price = null;
+        if($this->type==1){
+            if($this->membership_id!=null){
+                $membership = Membership::where('id','=',$this->membership_id)->first();
+                $membership_name = $membership->name;
+            }
+        }elseif($this->type==2){
+            if($this->membership_id!=null){
+                $nominal = NominalTopUp::where('id','=',$this->membership_id)->first();
+                $price = $nominal->harga;
+            }
         }
         return [
             'id' => $this->id,
             'user_id' => $this->user_id,
-            'nominal' => $this->nominal,
+            'nominal' => $price,
+            'harga' => $this->nominal,
             'payment_method' => $this->payment_method,
             'snap_token' => $this->snap_token,
             'status' => $this->status,
