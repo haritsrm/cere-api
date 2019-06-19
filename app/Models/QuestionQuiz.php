2ea1d81749b39fe7e740a122bb9657cb5ba32929
rietzche <haritzrahman98@gmail.com>
2019-06-19 19:34:25 +0700

    fix

--- app/Models/QuestionQuiz.php
+++ app/Models/QuestionQuiz.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class QuestionQuiz extends Model
 {
-    //
+    protected $fillable = [
+        'question', 'option_a', 'option_b', 'option_c', 'option_d', 'correct_answer', 'quiz_id'
+    ];
 }
